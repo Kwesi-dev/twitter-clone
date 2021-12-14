8f59b0b74e810e56d071121265c4ca9f88d25f8b
Kwesi Afriyie <afriyiesamuel36@gmail.com>
2021-12-13 16:56:46 -0800

    added some styling

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,8 +1,20 @@
 import "./profile.scss"
+import KeyboardBackspaceOutlinedIcon from '@mui/icons-material/KeyboardBackspaceOutlined'
 const Profile = () => {
     return (
         <div className="profile">
-            profile
+            <div className="profile__nav">
+                <KeyboardBackspaceOutlinedIcon className="profile__nav__icon"/>
+                <div className="profile__nav__item">
+                    <span>KwesiDev</span>
+                    <span>0 tweet</span>
+                </div>
+            </div>
+            <div className="profile__header">
+                <img src="https://i.ibb.co/LZm6JQb/khalid.jpg" alt="" className="profile__header__coverImg"/>
+                <img src="https://i.ibb.co/LZm6JQb/khalid.jpg" alt="" className="profile__header__img" />
+                <button>Edit profile</button>
+            </div>
         </div>
     )
 }
