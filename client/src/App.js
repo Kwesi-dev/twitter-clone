a0b2df5761a3a5d829cd8f2004e3ceccd7a334a3
Kwesi Afriyie <afriyiesamuel36@gmail.com>
2021-12-16 23:40:35 -0800

    added some styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,20 @@ import Home from './pages/home/Home'
 import LeftSideBar from "./components/leftSidebar/LeftSideBar"
 import RightSidebar from "./components/RightSideBar/RightSidebar"
 import Profile from "./pages/profile/Profile"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/login/Login'
 function App() {
+  const user = false
   return (
     <Router>
       <div className="app">
-        <LeftSideBar/>
+        {user && <LeftSideBar/>}
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/" element={user ? <Home/>: <Navigate to="/login"/>}/>
+          <Route path="/profile" element={user ? <Profile/> : <Navigate to="/login"/>}/>
+          <Route path="/login" element={user ? <Navigate to="/"/>: <Login/>}/>
         </Routes>
-        <RightSidebar/>
+        {user && <RightSidebar/>}
       </div>
     </Router>
   );
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,36 @@
+import './login.scss'
+import logo from '../../images/twitter.png'
+import google from '../../images/google.png'
+import apple from '../../images/apple.png'
+
+const Login = ()=>{
+    return(
+        <div className="login">
+            <div className="login__wrapper">
+                <img className="login__wrapper__logo" src={logo} alt=""/>
+                <span className="login__wrapper__header">Sign in to Twitter</span>
+                <div className="login__wrapper__google">
+                    <img src={google} alt="" />
+                    <span>Sign in with Google</span>
+                </div>
+                <div className="login__wrapper__apple">
+                    <img src={apple} alt="" />
+                    <span>Sign in with Apple</span>
+                </div>
+                <span className="or">or</span>
+                <form>
+                    <input type="text" placeholder="Phone, email or username"/>
+                    <button>Next</button>
+                </form>
+                <div className="login__wrapper__forgot">
+                    <span>Forgot password?</span>
+                </div>
+                <div className="login__wrapper__bottom">
+                    <span>Dont have an account?</span>
+                    <span>Sign up</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Login
